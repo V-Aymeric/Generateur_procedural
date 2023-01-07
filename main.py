e1415a99c2eb5e4f47512347d58d1825fbee193a
V-Aymeric <viala.aymeric@gmail.com>
2023-01-07 20:51:09 +0100

    Test commit for the first file

---
+++ main.py
@@ -0,0 +1,24 @@
+from kivy.app import App
+from Views.views_manager import get_main_window
+
+taille_tableau = 500
+
+
+class MainApp(App):
+    def build(self):
+        return get_main_window()
+
+
+def output_in_txt(arg_path, arg_str):
+    file = open(arg_path, "w", encoding="utf-8")
+    file.write(arg_str)
+    file.close()
+
+
+def main():
+    MainApp().run()
+
+
+if __name__ == '__main__':
+    main()
+
